3f34140592c85f59ecc63ab9888ef30e668d1546
y20-code <yls3235882107@gmail.com>
2025-04-14 16:41:01 +0800

    获取用户详细信息

--- src/main/java/com/yls/controller/UserController.java
+++ src/main/java/com/yls/controller/UserController.java
@@ -8,9 +8,7 @@ import com.yls.utils.Md5Util;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -60,5 +58,17 @@ public class UserController {
         }
 
         return Result.error("密码错误");
+    }
+
+    //获取用户信息
+    @GetMapping("userInfo")
+    public Result<User> userInfo(@RequestHeader(name = "Authorization") String token) {
+        //根据用户名查询用户信息
+        Map<String,Object> map = JwtUtil.parseToken(token);
+        String username = (String) map.get("username");
+
+        User user = userService.findByUserName(username);
+
+        return Result.success(user);
     }
 }
--- src/main/java/com/yls/pojo/User.java
+++ src/main/java/com/yls/pojo/User.java
@@ -2,6 +2,7 @@ package com.yls.pojo;
 
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -10,6 +11,7 @@ import java.time.LocalDateTime;
 public class User {
     private Integer id;//主键ID
     private String username;//用户名
+    @JsonIgnore //让springmvc把当前对象换成json字符串的时候，忽略password，最终的json字符串中就密钥password这个属性了
     private String password;//密码
     private String nickname;//昵称
     private String email;//邮箱
