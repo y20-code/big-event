314f124eaa391349a06931636d95d6adee615e96
y20-code <yls3235882107@gmail.com>
2025-04-14 10:18:34 +0800

    用户登录逻辑的实现

--- src/main/java/com/yls/controller/UserController.java
+++ src/main/java/com/yls/controller/UserController.java
@@ -3,6 +3,7 @@ package com.yls.controller;
 import com.yls.pojo.Result;
 import com.yls.pojo.User;
 import com.yls.service.UserService;
+import com.yls.utils.Md5Util;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -18,6 +19,7 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    //注册
     @PostMapping("register")
     public Result register(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$")String password) {
         //查询用户
@@ -30,7 +32,24 @@ public class UserController {
             //占用
             return Result.error("用户名以被占用");
         }
+    }
 
+    //登录
+    @PostMapping("/login")
+    public Result<String> login(String username, String password) {
+        //根据用户名查询用户
+        User loginUser = userService.findByUserName(username);
+        //判断该用户是否存在
+        if(loginUser==null){
+            return Result.error("用户不存在");
+        }
 
+        //判断密码是否正确
+        if(Md5Util.getMD5String(password).equals(loginUser.getPassword())){
+            //登录成功
+            return Result.success("jwt token令牌");
+        }
+
+        return Result.error("密码错误");
     }
 }
