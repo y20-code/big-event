4a54ff26412b14f20fbff9d52d540f4703b5ac10
y20-code <yls3235882107@gmail.com>
2025-04-14 08:40:32 +0800

    用户注册功能的实现

--- src/main/java/com/yls/pojo/Result.java
+++ src/main/java/com/yls/pojo/Result.java
@@ -4,10 +4,13 @@ package com.yls.pojo;
 //统一响应结果
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
+//No acceptable representation出现这个记得添加@Data
+@Data
 public class Result<T> {
     private Integer code;//业务状态码  0-成功  1-失败
     private String message;//提示信息
--- src/main/java/com/yls/service/impl/UserServiceImpl.java
+++ src/main/java/com/yls/service/impl/UserServiceImpl.java
@@ -1,18 +1,29 @@
 package com.yls.service.impl;
 
+import com.yls.mapper.UserMapper;
 import com.yls.pojo.User;
 import com.yls.service.UserService;
+import com.yls.utils.Md5Util;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
     public User findByUserName(String username) {
-        return null;
+        User u = userMapper.findzByUserName(username);
+        return u ;
     }
 
     @Override
     public void register(String username, String password) {
-
+        //加密
+        String md5String = Md5Util.getMD5String(password);
+        //添加
+        userMapper.add(username,md5String);
     }
 }
