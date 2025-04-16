719774ee1c6508ff9408b680c5720930e175f98d
y20-code <yls3235882107@gmail.com>
2025-04-16 19:57:44 +0800

    文件上传到oss

--- src/main/java/com/yls/controller/FileUploadController.java
+++ src/main/java/com/yls/controller/FileUploadController.java
@@ -1,6 +1,7 @@
 package com.yls.controller;
 
 import com.yls.pojo.Result;
+import com.yls.utils.AliOssutil;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,12 +14,13 @@ import java.util.UUID;
 public class FileUploadController {
 
     @PostMapping("/upload")
-    public Result<String> upload(MultipartFile file) throws IOException {
+    public Result<String> upload(MultipartFile file) throws Exception {
         //把文件的内容存储到本地磁盘上
         String originalFilename = file.getOriginalFilename();
         //保证文件的名字是唯一的，从而防止文件覆盖
         String filename = UUID.randomUUID().toString() + originalFilename.substring(originalFilename.lastIndexOf("."));
-        file.transferTo(new File("/Users/yanglinsen/Desktop/网页图片素材/"+filename));
-        return Result.success("url访问地址...");
+//        file.transferTo(new File("/Users/yanglinsen/Desktop/网页图片素材/"+filename));
+        String url = AliOssutil.uploadFile(filename,file.getInputStream());
+        return Result.success(url);
     }
 }
